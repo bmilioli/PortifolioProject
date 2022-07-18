a621d24304be70b13fd71ef2f7fb1d57375711b0
bmilioli <brunomiliolif@gmail.com>
2022-07-18 10:41:11 -0300

    Preloader

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+//Declarando variáveis
 var btnContact = document.querySelector('.jl-btn-contact');
 
-btnContact.addEventListener('click', function(){
+//Page preloader
+window.addEventListener('load', function () {
+    var pagePreloader = document.querySelector('.jl-preloader');
+    pagePreloader.classList.add('jl-fade-out');
+
+    setTimeout(function () {
+        pagePreloader.style.display = 'none';
+    }, 2000);
+});
+
+//Abrindo e fechando informações de Contato
+btnContact.addEventListener('click', function () {
     var boxContact = document.querySelector('.jl-contact-info');
     boxContact.classList.toggle('jl-is-open');
 
